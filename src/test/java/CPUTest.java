633a0a47f3bcdd395a339d86d763156c9b190795
VinceIP <vinceip532@gmail.com>
2024-06-08 12:06:25 -0400

    disabling test classes

--- src/test/java/CPUTest.java
+++ src/test/java/CPUTest.java
@@ -1,41 +1,41 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CPUTest {
-
-    @Test
-    void addWithCarry() {
-    }
-
-    @Test
-    void subWithCarry() {
-    }
-
-    @Test
-    void subWithCarryReverse() {
-    }
-
-    @Test
-    void logicalOR() {
-    }
-
-    @Test
-    void logicalAND() {
-    }
-
-    @Test
-    void logicalXOR() {
-    }
-
-    @Test
-    void bitshiftRight() {
-    }
-
-///    void bitshiftLeft() {
-//        CPU cpu = new CPU(new Display());
-//        cpu.registers.variableRegisters[0xF] = 50;
-//        cpu.bitshiftLeft(0xF);
-//        assertEquals(cpu.registers.variableRegisters[0xF], 0);
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class CPUTest {
+//
+//    @Test
+//    void addWithCarry() {
 //    }
-}
+//
+//    @Test
+//    void subWithCarry() {
+//    }
+//
+//    @Test
+//    void subWithCarryReverse() {
+//    }
+//
+//    @Test
+//    void logicalOR() {
+//    }
+//
+//    @Test
+//    void logicalAND() {
+//    }
+//
+//    @Test
+//    void logicalXOR() {
+//    }
+//
+//    @Test
+//    void bitshiftRight() {
+//    }
+//
+/////    void bitshiftLeft() {
+////        CPU cpu = new CPU(new Display());
+////        cpu.registers.variableRegisters[0xF] = 50;
+////        cpu.bitshiftLeft(0xF);
+////        assertEquals(cpu.registers.variableRegisters[0xF], 0);
+////    }
+//}
